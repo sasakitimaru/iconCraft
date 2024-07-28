afcad68b94dc7df010bd6909a9478c3a467be473
leaf-tkmmm <ydtkm1015@gmail.com>
2024-07-28 18:42:16 +0900

    feat: #15 ヘッダーテンプレートの追加

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 import { Heading } from "@chakra-ui/react";
+import Header from "./components/header.tsx";
 
 function App({ title, unko }: { title: string; unko?: string }) {
   return (
     <>
+      <Header />
       <Heading>
         Hello {title} {unko} !!
       </Heading>
